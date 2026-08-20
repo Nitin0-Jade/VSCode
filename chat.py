ccf7a293e88363a8679c66fc1f880cb31ef95934
Nitinraj Tiwari <nitinrajtiwari6@gmail.com>
2026-08-20 23:03:30 +0530

    First Code of cs50

--- chat.py
+++ chat.py
@@ -2,8 +2,12 @@ from openai import OpenAI
 
 client = OpenAI()
 
+user_prompt= input("Enter your prompt: ")
+system_prompt= "Limit to one word"
+
 response = client.responses.create(
-    input="are you AI?",
+    input=user_prompt,
+    instructions=system_prompt,
     model="gpt-4.1"
 )
 
